60af8d5c4723cf7fafedac8afda9c2cd46afc632
Harshita Choudhary <harshitachoudhary791@gmail.com>
2020-06-12 10:45:42 +0530

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Pokemon />
+  </div>
+</template>
+
+<script>
+import Pokemon from './components/Pokemon.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Pokemon
+  }
+}
+</script>
+
+<style lang="scss">
+@import url('https://use.fontawesome.com/releases/v5.8.2/css/all.css');
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+    margin: 0;
+    padding: 0;
+  }
+</style>
